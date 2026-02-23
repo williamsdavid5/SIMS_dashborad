2492438e132cb8d57d7a15bd7ffe5a347e99dfdd
David Duarte (Williams) <davidduart04@gmail.com>
2026-02-23 11:02:55 -0300

    criado o layout do dashboard, inclusive com responsividade

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,10 +7,46 @@ export default function Dashboard() {
             <main className="dashboardMain">
                 <MenuLateral></MenuLateral>
                 <section className='principalDash'>
-                    <div className='superiorDash'>
+                    <header className='superiorDash'>
                         <h2>Dashboard</h2>
                         <p>Feedback visual sobre o monitoramento, observe dados relevantes.</p>
-                    </div>
+                    </header>
+                    <section className='blocosDash'>
+                        <div className='bloco infoNumerica'>
+                            <p>Total hoje</p>
+                            <h1>28</h1>
+                        </div>
+                        <div className='bloco infoNumerica'>
+                            <p>Taxa de conformidade</p>
+                            <h1>91%</h1>
+                        </div>
+                        <div className='bloco infoNumerica cameraCritica'>
+                            <p>Câmera crítica</p>
+                            <h1>3</h1>
+                        </div>
+                        <div className='bloco distribuicaoPorCamera'>
+                            <p>Distribuição por câmera</p>
+                        </div>
+                        <div className='bloco inforOcorrenciasHora'>
+                            <p>Ocorrências por hora</p>
+                        </div>
+                        <div className='bloco falsosPositivos'>
+                            <p>Falsos positívos</p>
+                            <h1>3</h1>
+                        </div>
+                        <div className='bloco analiseEstatistica'>
+                            <p>Análise estatística</p>
+                        </div>
+                        <div className='bloco ocorrenciasPorTipo'>
+                            <p>Ocorrências por tipo</p>
+                        </div>
+                        <div className='bloco ultimasOcorrencias'>
+                            <p>Últimas ocorrências</p>
+                        </div>
+                        <div className='bloco historicoCompleto'>
+                            <p>Histórico completo</p>
+                        </div>
+                    </section>
                 </section>
             </main>
         </>
