72535ba243b968fca26e891bad78241ce00bdb51
David Duarte (Williams) <davidduart04@gmail.com>
2026-02-24 10:53:22 -0300

    pequenos ajustes de visualização. adição de uma tabela

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,7 +20,7 @@ import {
 
 export default function Dashboard() {
     const [totalHoje, setTotalHoje] = useState(27);
-    const [taxaConformidade, setTaxaConformidade] = useState(91);
+    const [taxaConformidade, setTaxaConformidade] = useState(91.23);
     const [cameraCritica, setCameraCritica] = useState("CAM03");
     const [falsosPositivos, setFalsosPositivos] = useState(4);
     const [intervaloMaisOcorrencias, setIntervaloMaisOcorrencias] = useState('10:00');
@@ -52,10 +52,95 @@ export default function Dashboard() {
     const distribuicaoPorCameraData = [
         { camera: "CAM01", Quantidade: 3 },
         { camera: "CAM02", Quantidade: 6 },
-        { camera: "CAM03", Quantidade: 7 }
+        { camera: "CAM03", Quantidade: 7 },
+        { camera: "CAM04", Quantidade: 4 }
     ]
 
-    const COLORS = ["#0A1E43", "#243757", "#364765", "#4F5F79"];
+    const ultimasOcorrencias = [
+        {
+            id: 1,
+            data: "2025-02-24T10:33:00",
+            camera: "CAM03",
+            item: "Capacete",
+            imagemUrl: "/imagens/oc1.jpg"
+        },
+        {
+            id: 2,
+            data: "2025-02-24T10:40:00",
+            camera: "CAM02",
+            item: "Luvas",
+            imagemUrl: "/imagens/oc2.jpg"
+        },
+        {
+            id: 3,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 4,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 5,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 6,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 7,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 8,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 9,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        }, {
+            id: 10,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        },
+        {
+            id: 11,
+            data: "2025-02-24T11:02:00",
+            camera: "CAM01",
+            item: "Óculos",
+            imagemUrl: "/imagens/oc3.jpg"
+        }
+    ];
+
+    const formatarData = (dataISO) => {
+        const data = new Date(dataISO);
+        return data.toLocaleString("pt-BR");
+    };
+
+    const COLORS = ["#0A1E43", "#243757", "#4b6185", "#879dc0"];
 
 
     return (
@@ -129,6 +214,15 @@ export default function Dashboard() {
                         </div>
                         <div className='bloco analiseEstatistica'>
                             <p className='tituloBloco'>Análise estatística</p>
+                            <hr />
+                            <p className='inforEstatistica'>
+                                <b>Média diária:</b> 17 Ocorrências <br />
+                                <b>Total para esta semana: </b> 29 Ocorrências <br />
+                                <b>Média semanal:</b> 23 Ocorrências <br />
+                                <b>Média de tempo tem EPI:</b> 2m 23s <br />
+                                <b>EPI mais crítico:</b> Óculos <br />
+                                <b>Câmera com maior incidência:</b> CAM03 <br />
+                            </p>
                         </div>
                         <div className='bloco ocorrenciasPorTipo'>
                             <p className='tituloBloco'>Ocorrências por tipo</p>
@@ -151,6 +245,31 @@ export default function Dashboard() {
                         </div> */}
                         <div className='bloco ultimasOcorrencias'>
                             <p className='tituloBloco'>Últimas ocorrências</p>
+                            <table border={1} className='tabelaUltimasOcorrencias'>
+                                <thead>
+                                    <tr>
+                                        <th>Data</th>
+                                        <th>Câmera</th>
+                                        <th>Item envolvido</th>
+                                        <th>Imagem</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {ultimasOcorrencias.map((ocorrencia) => (
+                                        <tr key={ocorrencia.id}>
+                                            <td>{formatarData(ocorrencia.data)}</td>
+                                            <td>{ocorrencia.camera}</td>
+                                            <td>{ocorrencia.item}</td>
+                                            <td>
+                                                <a href={ocorrencia.imagemUrl} target="_blank" rel="noopener noreferrer">
+                                                    Ver
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                         <div className='bloco historicoCompleto'>
                             <p className='tituloBloco'>Histórico completo</p>
