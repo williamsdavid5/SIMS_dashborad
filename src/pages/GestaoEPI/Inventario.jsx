1a24b91375fea980e1087753293e5ddfafed777a
David Duarte (Williams) <davidduart04@gmail.com>
2026-03-03 10:59:06 -0300

    ajustes no layout dos EPIs, ainda sujeito a modificações

--- src/pages/GestaoEPI/Inventario.jsx
+++ src/pages/GestaoEPI/Inventario.jsx
@@ -7,11 +7,68 @@ import Luvas from '../../assets/epis/luvas.jpg'
 import Mascara from '../../assets/epis/mascara.jpg'
 import Botas from '../../assets/epis/botas.jpeg'
 
+const epi_lista = [
+    {
+        nome: 'Capacete',
+        total: '17',
+        disponiveis: '2',
+        imagem: Capacete  // Sem chaves, pois já é a variável importada
+    },
+    {
+        nome: 'Óculos de Proteção',
+        total: '32',
+        disponiveis: '8',
+        imagem: Oculos
+    },
+    {
+        nome: 'Colete Reflexivo',
+        total: '25',
+        disponiveis: '5',
+        imagem: Colete
+    },
+    {
+        nome: 'Protetor Auricular',
+        total: '41',
+        disponiveis: '12',
+        imagem: Protetor_auricular
+    },
+    {
+        nome: 'Luvas de Raspa',
+        total: '53',
+        disponiveis: '15',
+        imagem: Luvas
+    },
+    {
+        nome: 'Máscara PFF2',
+        total: '28',
+        disponiveis: '7',
+        imagem: Mascara
+    },
+    {
+        nome: 'Botas de Segurança',
+        total: '36',
+        disponiveis: '9',
+        imagem: Botas
+    }
+]
+
 export default function Inventario() {
     return (
         <>
             <section className="episBlocos">
-                <div className="blocoEpi">
+                {epi_lista.map((epi) => {
+                    return (
+                        <>
+                            <div className="blocoEpi">
+                                <h3>{epi.nome}</h3>
+                                <img src={epi.imagem} alt="" />
+                                <p>Total: {epi.total}</p>
+                                <p>Disponíveis: {epi.disponiveis}</p>
+                            </div>
+                        </>
+                    )
+                })}
+                {/* <div className="blocoEpi">
                     <img src={Capacete} alt="" />
                     <h3>Capacete</h3>
                     <p>Total: 17</p>
@@ -52,7 +109,7 @@ export default function Inventario() {
                     <h3>Botas</h3>
                     <p>Total: 17</p>
                     <p>Disponíveis: 8</p>
-                </div>
+                </div> */}
             </section>
         </>
     )
