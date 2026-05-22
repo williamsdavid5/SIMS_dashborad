03adccca7b1a32c56190cb3f55cc30c07ab31fc6
David Duarte (Williams) <davidduart04@gmail.com>
2026-05-21 21:05:36 -0300

    emovido o timeout de requisição da API. tentativa de ajustar a responsividade para telas maiores

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'https://api-epi-metrics.onrender.com/api',
-    timeout: 10000,
+    // timeout: 10000,
 });
 
 export default api;
